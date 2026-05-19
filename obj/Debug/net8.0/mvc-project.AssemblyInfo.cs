d54ca7ffc77bb33fdb952feb998f552a48757c1f
Abdulrahman Nasser <abdulrahman.nasser222@gmail.com>
2026-05-19 23:40:11 +0300

    Fix dark mode styling for task edit page form elements

--- obj/Debug/net8.0/mvc-project.AssemblyInfo.cs
+++ obj/Debug/net8.0/mvc-project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mvc-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb00005e3a27284ebab01028750c111980e6e706")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d3373aca0bd71195cdb93eb6f10029ea0b4ba30")]
 [assembly: System.Reflection.AssemblyProductAttribute("mvc-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mvc-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
